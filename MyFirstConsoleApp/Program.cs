353561f3d34fa9a4a8a2c17fb04d854c60f00fce
augieamurao <33384474+augieamurao@users.noreply.github.com>
2018-01-18 12:57:19 -0800

    Initial commit

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World");
+
+            Messenger msg = new Messenger("Hello Augie");
+            msg.author = "Cindy";
+
+            Console.WriteLine(msg.SayHello());
+            Console.ReadLine();
+        }
+    }
+    class Messenger
+    {
+        private string message;
+        public string author { get; set; }
+
+        public Messenger(string msg)
+        {
+            message = msg;
+            return;
+        }
+        public string SayHello()
+        {
+            return message + " from " + author;
+        }
+    }
+}
